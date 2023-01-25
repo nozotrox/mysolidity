410cd58cb01b8b6312ae59a1af3238ccc1cf992c
nozotrox <feliciano_jr.mazoio@yahoo.mail>
2023-01-25 22:18:36 +0200

    refactor: changed the smart wallet app implementation

--- smart_wallet.sol
+++ smart_wallet.sol
@@ -1,47 +1,70 @@
+//SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.17;
 
-contract SmartWallet { 
+contract Consumer { 
 
-    struct wallet { 
-        address owner;
-        uint balance;
-        address[] guardians;
-        mapping(address => uint) allowances;
-        uint8 nAllowances;
+    function getBalance() external view returns(uint) { 
+        return address(this).balance;
     }
 
-    fallback() external payable { 
-        wallets[msg.sender].balance += msg.value;
-    }
-
-    mapping(address => wallet) public wallets;
-
-    function createWallet() external payable { 
-        wallet storage newWallet = wallets[msg.sender];
-        newWallet.owner = msg.sender;
-        newWallet.balance += msg.value;
-    }
-
-    function sendMoney(address payable to) external payable { 
-        // safety Check
-        uint transferValue = msg.value;
-        wallet storage senderWallet = wallets[msg.sender];
-        require(transferValue <= senderWallet.balance, "Not enougth funds");
-        require(senderWallet.guardians.length <= 5 && senderWallet.guardians.length >=3, "Cannot perform operation. Insufficient number of guardians");
-        senderWallet.balance -= transferValue;
-        // Actual Operation
-        to.transfer(transferValue);
-    }
-
-    function setAllowance(address to, uint amount) external { 
-        wallet storage senderWallet = wallets[msg.sender];
-        senderWallet.allowances[to] = amount;
-    }
-
-    function addGuardian(address guardian) external {
-        wallet storage senderWallet = wallets[msg.sender];
-        assert(msg.sender == senderWallet.owner);
-        senderWallet.guardians.push(guardian);
-        require(senderWallet.guardians.length <= 5, "Maximum number of guardians reached (5)");
-    }
+    function deposit() external payable {}
 }
+
+contract SmartContract { 
+
+    address public owner;
+    
+    mapping (address => uint) allowances;
+    mapping (address => bool) guardians;
+    mapping (address => mapping(address => bool)) newOwnerVoters;
+    address nextOwner;
+    uint8 constant newOwnerVoteResetCount = 3;
+    uint8 newOwenerVotes;
+
+    constructor() { 
+        owner = msg.sender;
+    }
+
+    function voteNewOwner(address _newProposedOwner) external { 
+        require(guardians[msg.sender], "You are not a guardian or you voting was disabled");
+        require(!newOwnerVoters[_newProposedOwner][msg.sender], "You've already voted for this new proposed owener. Aborting...");
+        
+        if(nextOwner != _newProposedOwner) { 
+            nextOwner = _newProposedOwner;
+            newOwenerVotes = 0;
+        }
+
+        newOwenerVotes++;
+        newOwnerVoters[_newProposedOwner][msg.sender] = true;
+        if(newOwenerVotes >= newOwnerVoteResetCount) { 
+            owner = nextOwner;
+            nextOwner = address(0);
+        }
+    }
+
+    function setGuardian(address _guardian, bool _isEnabled) external { 
+        require(msg.sender == owner, "You are not the owner. Aborting...");
+        guardians[_guardian] = _isEnabled;
+    }
+
+    function setAllowance(address _for, uint _amount) external { 
+        require(msg.sender == owner, "You are not the owner. Aborting...");
+        allowances[_for] = _amount;
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) external payable returns(bytes memory){ 
+        if(msg.sender != owner) { 
+            // :: Only transfer the allowed balance for the user
+            require(_amount > 0, "Invalid amount to transfer, aborting...");
+            require(allowances[msg.sender] >= _amount, "Amount greater then allowance of user, aborting...");
+
+            allowances[msg.sender] -= _amount;
+        }
+
+        (bool isSuccess, bytes memory responseData) = _to.call{value: _amount}(_payload);
+        return responseData;
+    }
+
+}
+
